074e17b717325106ef0357921046889c437a7580
rahulsingh0806 <rahulsinghmar1@gmail.com>
2025-06-20 09:08:43 +0530

    Initial commit

--- Security/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = express();
-const router = express.Router();
-
-const {signUp, login} = require("../controllers/Auth")
-
-router.post("/login",login);
-router.post("/signup", signUp);
-
-module.exports = router;
